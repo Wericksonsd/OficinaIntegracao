edcea722e7417e937f33f716b38d48c208ed38b2
Wericksonsd <wericksonsdl@gmail.com>
2025-10-21 16:10:31 -0300

    asdas

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-[100dvh] w-full flex-col items-center justify-center p-12 bg-mainBlue">
+    <div className="flex min-h-dvh w-full flex-col items-center justify-center p-12">
       <div className="flex items-center justify-center w-full h-60">
         <Image
           src="/horBranca.png"
@@ -14,7 +14,7 @@ export default function Home() {
       </div>
 
       <LoginForm/>
-      
+
     </div>
   );
 }
--- my-app/src/components/loginForm.tsx
+++ my-app/src/components/loginForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { signIn } from "next-auth/react";
+import { User } from "lucide-react";
 
 export default function LoginForm() {
 
@@ -18,19 +19,41 @@ export default function LoginForm() {
     }
 
 return (
-    <form onSubmit={login} className="flex flex-col items-center justify-center gap-2 w-full mt-12 p-12 flex-1 bg-zinc-700 rounded-lg" >       
-        <input
-            name="email"
-            type="email"
-            placeholder="E-mail"
-            className="input input-primary"
-        />
-        <input
-            name="password"
-            type="password"
-            placeholder="Senha"
-            className="input input-primary"
-        />
+    <form onSubmit={login} className="flex flex-col items-center justify-start gap-4 w-full mt-12 p-8 flex-1 bg-zinc-100 rounded-lg" >
+
+        <div className="w-full flex">
+            <div className="w-1/2 text-3xl text-[#211f45] font-bold">
+                <p>LOGIN</p>
+                <div className="w-full h-1 bg-[#211f45] rounded-l-lg"></div>
+            </div>
+            <div className="w-1/2 text-3xl text-zinc-500 font-bold text-right">
+                <p>LOGIN</p>
+                <div className="w-full h-1 bg-zinc-500 rounded-r-lg"></div>
+            </div>
+        </div>
+
+        <p className="text-[#211f45] text-center flex-1">Acesse utilizando seu e-mail institucional. <br /> Caso seja seu primeiro acesso, crie sua conta.</p>
+
+        <div className="w-full flex item gap-4 p-4 rounded-lg text-zinc-300 border-2 border-zinc-300 hover:bg-zinc-300 hover:text-[#211F45]">
+            <User />
+            <input
+                name="email"
+                type="email"
+                placeholder="E-mail"
+                className="flex-1 bg-transparent outline-none"
+            />
+        </div>
+
+        <div className="w-full flex item gap-4 p-4 rounded-lg text-zinc-300 border-2 border-zinc-300 hover:bg-zinc-300 hover:text-[#211F45]">
+            <User />
+            <input
+                name="password"
+                type="password"
+                placeholder="Senha"
+                className="flex-1 bg-transparent outline-none"
+            />
+        </div>
+
         <button id="logar" className="btn btn-primary w-full mt-6" type="submit"> logar </button>
     </form>
 );
