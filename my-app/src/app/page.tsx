dee03d45ed86464e1de87ffc1e10ee986a5eb63c
Wericksonsd <wericksonsdl@gmail.com>
2025-10-22 17:05:34 -0300

    nova

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-dvh w-full flex-col items-center justify-center p-12">
+    <div className="flex max-h-dvh w-full flex-col items-center justify-center p-12">
       <div className="flex items-center justify-center w-full h-60">
         <Image
           src="/horBranca.png"
           alt="Logo AP"
           width={300}
-          height={100}
+          height={60}
         />
       </div>
 
